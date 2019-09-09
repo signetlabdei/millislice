b790529d1f9d69673895591611eca29b660d1eb4
Pagin Matteo <mattpagg@gmail.com>
2019-09-09 11:31:26 +0200

    Initial params specification

--- scratch/simulation-config/simulation-config.h
+++ scratch/simulation-config/simulation-config.h
@@ -26,6 +26,7 @@ public:
   static void SetConstantVelocityMobility(Ptr<Node> node, Vector position, Vector velocity);
   static void SetRandomWalkMobility(Ptr<Node> node, Vector position, double vMin, double vMax);
   static void SetupUdpApplication(Ptr<Node> node, Ipv4Address address, uint16_t port, uint16_t interPacketInterval, double startTime, double endTime);
+  static double RateToIPI(uint16_t appRate, UdpClientHelper udpClient);
   static void SetupFtpModel3Application(Ptr<Node> clientNode, Ptr<Node> serverNode, Ipv4Address address, uint16_t port, double lambda, uint32_t fileSize, uint32_t sendSize, double startTime, double endTime, Ptr<OutputStreamWrapper> stream);
   static void SetupUdpPacketSink(Ptr<Node> node, uint16_t port, double startTime, double endTime, Ptr<OutputStreamWrapper> stream);
   static void InstallRmcatApps(bool nada, Ptr<Node> sender, Ptr<Node> receiver, uint16_t port, float initBw, float minBw, float maxBw, float startTime, float stopTime);
--- sem_script.py
+++ sem_script.py
@@ -1,7 +1,27 @@
 import sem
 ns_path = './'
-ns_script = 'slicing'    
+ns_script = 'slicing'
 ns_res_path = './slicing-res'
 
 # Create the actual simulation campagins
-campaign = sem.CampaignManager.new(ns_path, ns_script, ns_res_path, check_repo=False)
+campaign = sem.CampaignManager.new(
+    ns_path, ns_script, ns_res_path, check_repo=False)
+
+print(campaign)
+
+params_grid = {
+    'bw': 5e8,
+    'f0': 10e9,  # URLCC's CC
+    'f1': 28e9,  # eMBB's CC
+    'mode': [1, 2], # Test both without/with CA slicing 
+    'numEmbbUes': 10,
+    'numUrllcUes': 10,
+    'scenario': 'test-single-enb-moving',
+    'scheduler': 1,  # Round Robin Scheduler
+    'simTime': 2.0,  # Low just for testing purposes, then at least 10
+    'useBuildings': False,  # Use MmWave3gppPropagationLossModel
+    'useRlcAm': False,  # Use RLC UM
+    'úseUdp': True,
+    'vMax': 1.0,
+    'vMin': 10.0,
+}
