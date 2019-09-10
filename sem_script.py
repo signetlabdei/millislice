f3759f59a47c031199522525d0227f55d2f13052
Pagin Matteo <mattpagg@gmail.com>
2019-09-10 10:57:59 +0200

    Values fix

--- sem_script.py
+++ sem_script.py
@@ -7,22 +7,22 @@ ns_res_path = './slicing-res'
 campaign = sem.CampaignManager.new(
     ns_path, ns_script, ns_res_path, check_repo=False)
 
-print(campaign)
 # Obtain IPIs from rates
 # eMMB
 embb_packet_size = 1024
-embb_rate_grid = range(80, 120, 10)
+embb_rate_grid = list(range(80, 120, 10))
+embb_IPI_grid = []
 for rate in embb_rate_grid:
-    rate = embb_packet_size*8/(rate*1e6)
-embb_IPI_grid = list(embb_rate_grid)
+    embb_IPI_grid.append(embb_packet_size*8/(rate*1e6))
 # URLLC
 urllc_packet_size = 1024
-urllc_rate_grid = range(1, 1, 1)
+urllc_rate_grid = list(range(1, 2, 1))
+urllc_IPI_grid = []
 for rate in urllc_rate_grid:
-    rate = urllc_packet_size*8/(rate*1e6)
-urllc_IPI_grid = list(urllc_rate_grid)
+    urllc_IPI_grid.append(urllc_packet_size*8/(rate*1e6))
 
-sim_duration = 2.0
+# Set amount of simulation time
+sim_duration = 3.0
 
 params_grid = {
     'appEnd': sim_duration,
@@ -35,7 +35,7 @@ params_grid = {
     'embbUdpIPI': embb_IPI_grid,
     'f0': 10e9,  # URLCC's CC
     'f1': 28e9,  # eMBB's CC
-    'filePath': 'sim_res',
+    'filePath': 'test_',
     'fileSize': 512000,
     'lambdaUrllc': 0.2,
     'mode': [1, 2],  # Test both without/with CA slicing
@@ -49,7 +49,7 @@ params_grid = {
     'scheduler': 1,  # Round Robin Scheduler
     'simTime': sim_duration,  # Low just for testing purposes, then at least 10
     'urllcOn': True,
-    'urllcUdpIPI': 1,
+    'urllcUdpIPI': urllc_IPI_grid,
     'useBuildings': False,  # Use MmWave3gppPropagationLossModel
     'useRlcAm': False,  # Use RLC UM
     'useUdp': True,
@@ -57,10 +57,10 @@ params_grid = {
     'vMin': 10.0,
 }
 
+print(params_grid)
 runs = 1
 campaign.run_missing_simulations(sem.list_param_combinations(params_grid), runs)
 
 # Get missing results for no CA and CC equal to 28GHz
 params_grid.update(mode=1, f0=28e9)
-
 campaign.run_missing_simulations(sem.list_param_combinations(params_grid), runs)
