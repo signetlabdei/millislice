5434a54ace7307eda10fb69780ecbc711a0ee1db
Pagin Matteo <mattpagg@gmail.com>
2019-09-12 15:14:25 +0200

    Res loading structure

--- metrics.py
+++ metrics.py
@@ -6,21 +6,22 @@ campaign = sem.CampaignManager.load('./slicing-res')
 print(campaign)
 
 #results = sem.CampaignManager.get_results_as_numpy_array()
-#print(results)
-
-def throughput(dict)
-for result in [campaign.db.get_complete_results({'nDevices': 4000})[0]]:
-
-       
-
-        
-
-    
+# print(results)
 
 
+def throughput(dict):
+    for result in [campaign.db.get_complete_results({'nDevices': 4000})[0]]:
+        print(result)
 
 
+def delay_app():
+    results = campaign.db.get_complete_results()
+    # Get the required output traces
+    trace_bucket = []
+    for result in results:
+        dl_app_trace = result['output']['test_eMBB-dl-app-trace.txt']
+        trace_bucket.append(dl_app_trace)
+        print(dl_app_trace)
+        print('---------------------')
 
-
-
-
+delay_app()
