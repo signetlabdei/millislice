fa570df58db9efee01aa6beea5ca1380d7701c56
fra_ncesco <francesco.agostini1993@outlook.it>
2019-09-12 14:00:16 +0200

    added metrics.py

---
+++ metrics.py
@@ -0,0 +1,26 @@
+import sem
+import numpy as np
+import os
+import matplotlib
+campaign = sem.CampaignManager.load('./slicing-res')
+print(campaign)
+
+#results = sem.CampaignManager.get_results_as_numpy_array()
+#print(results)
+
+def throughput(dict)
+for result in [campaign.db.get_complete_results({'nDevices': 4000})[0]]:
+
+       
+
+        
+
+    
+
+
+
+
+
+
+
+
